ecdef3d432b95d610ffb5f14739787efd8f54714
Manu <manusolerj@gmail.com>
2026-09-18 19:19:55 +0200

    feat: add server component with HTML template placeholder

---
+++ src/app/features/server/server.ts
@@ -0,0 +1,9 @@
+import { Component, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'app-server',
+  imports: [],
+  templateUrl: './server.html',
+  changeDetection: ChangeDetectionStrategy.OnPush,
+})
+export class Server {}
