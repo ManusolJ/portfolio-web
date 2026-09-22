ac1a7af1a04605f044c8247d161aa7db0c33c906
Manu <manusolerj@gmail.com>
2026-09-22 20:34:07 +0200

    feat: implement placeholder for server feature  with title card and placeholder icon, add localization entries

--- src/app/features/server/server.ts
+++ src/app/features/server/server.ts
@@ -1,9 +1,20 @@
 import { Component, ChangeDetectionStrategy } from '@angular/core';
+import { NgIcon } from '@ng-icons/core';
+import { lucideAlertTriangle, lucideServer } from '@ng-icons/lucide';
+import { TitleCard } from '@shared/components/title-card/title-card';
 
 @Component({
+  imports: [TitleCard, NgIcon],
   selector: 'app-server',
-  imports: [],
   templateUrl: './server.html',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Server {}
+export class Server {
+  protected readonly icon = lucideServer;
+
+  protected readonly pretitle = $localize`:@@server.pretitle:Servidor`;
+  protected readonly title = $localize`:@@server.title:Lo que he desplegado`;
+
+  protected readonly placeholderIcon = lucideAlertTriangle;
+  protected readonly placeholder = $localize`:@@server.placeholder:¡Esta función llegará más adelante, cuando el backend esté listo!`;
+}
